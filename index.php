69c2d64f6dc8883b2924495265f93c8751ef4faa
Ildar Bikmamatov <vistoyn@gmail.com>
2020-12-07 15:35:44 +0000

    template fix

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-$context = Timber::context();
-//$context['posts'] = new Timber\PostQuery();
-Timber::render( 'pages/index.twig', $context );
+global $timber_site;
+if ($timber_site)
+{
+	$timber_site->route_render();
+}
+else
+{
+	$context = Timber::context();
+	//$context['posts'] = new Timber\PostQuery();
+	Timber::render( 'pages/index.twig', $context );
+}
