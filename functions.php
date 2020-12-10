83b88e07a210be51a46038ad905f8fd185137eee
Ildar Bikmamatov <ildar@bayrell.org>
2020-12-10 14:30:03 +0000

    add jquery

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ include __DIR__ . "/admin/include.php";
 
 define ( 'THEME_WP_SHOW', true );
 
-if ( ! class_exists( 'Timber' ) ) {
+if ( ! class_exists( 'Timber' ) or ! class_exists( 'Elberos\Site' ) ) {
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
 	});
