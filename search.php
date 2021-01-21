bfecf7d3eb4434a74efa17070924a055779bc975
Ildar Bikmamatov <ildar@bayrell.org>
2021-01-21 12:32:50 +0000

    search fix

--- search.php
+++ search.php
@@ -1,8 +1,8 @@
 <?php
 
 $context = Timber::context();
-$args = array( 'posts_per_page' => 4, 's'=>isset($_GET['s'])?$_GET['s']:'' );
+$args = array( 'posts_per_page' => 10, 's'=>isset($_GET['s'])?$_GET['s']:'' );
 $context['query'] = $args;
 $query = new Timber\PostQuery( $args );
-$context['posts'] = $query->get_posts();
+$context['posts'] = $query;
 Timber::render( 'pages/category.twig', $context );
