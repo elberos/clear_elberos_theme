9eca8635af2cfa9708d0e44d972141b0490ce1b9
Ildar Bikmamatov <ildar@bayrell.org>
2021-06-09 11:36:32 +0000

    some fix

--- page.php
+++ page.php
@@ -1,12 +1,1 @@
-<?php
-
-global $site_template;
-$site_template->context["post"] = $site_template->post;
-
-/* Render page */
-echo $site_template->render_page
-([
-	'pages/page_' . $site_template->post->ID . '.twig',
-	'pages/post_type_' . $site_template->post->post_type . '.twig',
-	'pages/page.twig'
-]);
+<?php include("single.php");
--- single.php
+++ single.php
@@ -8,14 +8,15 @@ if ( post_password_required( $site_template->post->ID ) )
 	echo $site_template->render_page
 	([
 		'pages/page_password.twig',
+		'pages/page.twig'
 	]);
 }
 else
 {
 	echo $site_template->render_page
 	([
-		'pages/page_' . $site_template->post->ID . '.twig',
-		'pages/post_type_' . $site_template->post->post_type . '.twig',
+		'pages/page-' . $site_template->post->ID . '.twig',
+		'pages/page-' . $site_template->post->post_type . '.twig',
 		'pages/page.twig'
 	]);
 }
